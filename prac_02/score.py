22316f6eb5f86957371f3080c89c893544f3a7c8
15jwarde <jorja.warde@my.jcu.edu.au>
2023-09-22 22:21:40 +1000

    Refactor scores program to use functions

---
+++ prac_02/score.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    result = get_result(score)
+    random_result = get_result(random.randint(0, 100))
+    print(f"Your result: {result}\nRandom result: {random_result}")
+
+
+def get_result(number):
+    if number < 50:
+        return "Bad"
+    elif number < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
