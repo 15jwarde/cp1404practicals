e9e81ed69ec49ff9116380b04cb32c32bd6810a4
15jwarde <jorja.warde@my.jcu.edu.au>
2023-11-16 01:51:32 +1000

    Add docstrings to methods within the Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -11,15 +11,19 @@ VINTAGE_AGE = 50
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise Guitar."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string with Guitar attributes."""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def get_age(self):
+        """Calculate age of the Guitar."""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self, year):
+        """Determine if guitar is vintage based on age."""
         return self.get_age() >= VINTAGE_AGE
